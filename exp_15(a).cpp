df0da7775b515cecb69296459019f770274997d6
adity365 <aditya.agarwal.20.08.2002@gmail.com>
2024-10-01 17:18:54 +0530

    Update exp_15(a).cpp

--- exp_15(a).cpp
+++ exp_15(a).cpp
@@ -2,21 +2,25 @@
 // PRN --> 23070123162
 
 // code to find out factorial of a number 
-# include <iostream>
+#include <iostream>
 using namespace std;
 
-int fact(int n){
-    if(n<=1){// Terminating statement(base condition)
-    return 1;
-    }
-    else{
-        return (n*fact(n-1)); // Recursion
+int fact(int n) {
+    if (n <= 1) { // Base condition
+        return 1;
+    } else {
+        return (n * fact(n - 1)); // Recursive call
     }
 }
 
-int main(){
-
-    int X,n;
-    cout << "Enter a number : " << endl;
+int main() {
+    int n;
+    cout << "Enter a number: " << endl;
     cin >> n;
+
+    // Calling the fact function and printing the result
+    int result = fact(n);
+    cout << "Factorial of " << n << " is: " << result << endl;
+
+    return 0;
 }
